7eb7621c3c2412db3a2bd047693b5974ccfc7ae8
Anusha Datar <anushadatar@gmail.com>
2019-02-03 14:41:00 -0500

    some directionality

--- App/app/src/main/java/com/example/gotyourback/MapsActivity.java
+++ App/app/src/main/java/com/example/gotyourback/MapsActivity.java
@@ -1,4 +1,3 @@
-// Heavily adapted from https://android.jlelse.eu/google-maps-directions-api-5b2e11dee9b0
 package com.example.gotyourback;
 
 import android.os.Bundle;
@@ -14,6 +13,7 @@ import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.maps.android.PolyUtil;
 import com.google.maps.DirectionsApi;
 import com.google.maps.GeoApiContext;
 import com.google.maps.errors.ApiException;
@@ -24,6 +24,7 @@ import com.google.maps.model.TravelMode;
 import org.joda.time.DateTime;
 
 import java.io.IOException;
+import java.lang.reflect.Array;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
@@ -70,12 +71,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         setupGoogleMapScreenSettings(googleMap);
         // Hardcode in the places to go.
-        DirectionsResult results = getDirectionsDetails("52 Oxford St, Cambridge, MA 02138","1000 Olin Way, Needham, MA 02492",TravelMode.WALKING);
+        DirectionsResult results = getDirectionsDetails("52 Oxford St, Cambridge, MA 02138","1000 Olin Way Needham, MA 02492",TravelMode.WALKING);
         if (results != null) {
             addPolyline(results, googleMap);
+            List<LatLng> path = getDecodedPath(results, googleMap);
             positionCamera(results.routes[overview], googleMap);
             addMarkersToMap(results, googleMap);
             Log.i("Directions Results.", results.routes[0].legs[0].duration.humanReadable);
+            startTravel(path);
         }
     }
 
@@ -103,22 +106,51 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
     private void addPolyline(DirectionsResult results, GoogleMap mMap) {
-     //   List<LatLng> decodedPath = PolyUtil.decode(results.routes[overview].overviewPolyline.getEncodedPath())
-        // mMap.addPolyline(new PolylineOptions().addAll(decodedPath));
-        Log.i("Polyline","Imagine a polyline.");
+        List<LatLng> decodedPath = PolyUtil.decode(results.routes[overview].overviewPolyline.getEncodedPath());
+        mMap.addPolyline(new PolylineOptions().addAll(decodedPath));
+
+    }
+    private List<LatLng> getDecodedPath(DirectionsResult results, GoogleMap mMap) {
+        List<LatLng> decodedPath = PolyUtil.decode(results.routes[overview].overviewPolyline.getEncodedPath());
+        return decodedPath;
     }
 
     private String getEndLocationTitle(DirectionsResult results){
         return  "Time :"+ results.routes[overview].legs[overview].duration.humanReadable + " Distance :" + results.routes[overview].legs[overview].distance.humanReadable;
     }
 
+    private void startTravel(List<LatLng> decodedPath) {
+        // Generate a list of directions.
+        double[][] directions = new double[decodedPath.size()][2];
+        // This measurement is in miles.
+        int earth_radius = 3959;
+        for (int i=5; i < decodedPath.size()/5; i++) {
+            // Use the Haversine formula to get distances.
+            i *= 5;
+            double longitude_difference = Math.toRadians(decodedPath.get(i).latitude - decodedPath.get(i-5).latitude);
+            double latitude_difference = Math.toRadians(decodedPath.get(i).longitude - decodedPath.get(i-5).longitude);
+  //          double distance_total = Math.sqrt(Math.pow(latitude_difference, 2.0) + Math.pow(longitude_difference, 2.0));
+            double imm = Math.pow((Math.sin(latitude_difference/2)), 2) + Math.cos(Math.toRadians(decodedPath.get(i).latitude)) * Math.cos(Math.toRadians(decodedPath.get(i-5).latitude)) * Math.pow(Math.sin(longitude_difference/2),2);
+            double scaled_imm = 2 * Math.atan2( Math.sqrt(imm), Math.sqrt(1-imm));
+            double distance = earth_radius * scaled_imm;
+            double theta = Math.toDegrees(Math.atan2(Math.sin(longitude_difference) * Math.cos(Math.toRadians(decodedPath.get(i).latitude)), Math.cos(Math.toRadians(decodedPath.get(i-5).latitude)) * Math.sin(Math.toRadians(decodedPath.get(i).latitude)) - Math.sin(Math.toRadians(decodedPath.get(i-5).latitude)) * Math.cos(Math.toRadians(decodedPath.get(i).latitude)) * Math.cos(longitude_difference)));
+            directions[i][0] = distance;
+            directions[i][1] = theta;
+            Log.i("distance", Double.toString(distance));
+
+            Log.i("angle", Double.toString(theta));
+        }
+        Log.i("directions array", Double.toString(directions.length));
+    }
+
     private GeoApiContext getGeoContext() {
         GeoApiContext geoApiContext = new GeoApiContext();
         return geoApiContext
                 .setQueryRateLimit(3)
-                .setApiKey("KEY_GOES_HERE")
+                .setApiKey("API_KEY_HERE")
                 .setConnectTimeout(1, TimeUnit.SECONDS)
                 .setReadTimeout(1, TimeUnit.SECONDS)
                 .setWriteTimeout(1, TimeUnit.SECONDS);
     }
+
 }
